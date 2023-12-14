9bf459dfb10cce5808282c9f86b34a6e70aa783d
IsaacGalindoBarbina <20203tn019@utez.edu.mx>
2023-12-14 16:06:26 -0600

    service worker

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './assets/base.css';
+import 'react-bootstrap-typeahead/css/Typeahead.css';
+import Main from './DemoPages/Main';
+import configureStore from './config/configureStore';
+import { Provider } from 'react-redux';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+const store = configureStore();
+const rootElement = document.getElementById('root');
+
+const renderApp = (Component) => {
+	ReactDOM.render(
+		<Provider store={store}>
+			<BrowserRouter>
+				<Component />
+			</BrowserRouter>
+		</Provider>,
+		rootElement
+	);
+};
+serviceWorkerRegistration.register({
+	onUpdate: async (registration) => {
+		if (registration && registration.waiting) {
+			await registration.unregister();
+			registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+
+			window.location.reload();
+		}
+	},
+});
+renderApp(Main);
