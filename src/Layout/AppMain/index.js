aaf5090ebc5bad60d74b4e655abedf91b6240139
IsaacGalindoBarbina <20203tn019@utez.edu.mx>
2023-12-14 22:45:16 -0600

    change in index.js

--- src/Layout/AppMain/index.js
+++ src/Layout/AppMain/index.js
@@ -1,5 +1,10 @@
 import React, { Suspense, Fragment } from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Redirect,
+} from 'react-router-dom';
 import { AuthProvider, useAuth } from './AuthContext';
 import Login from '../../components/login';
 import RouteConstant from '../../router/routeConstant';
@@ -7,49 +12,61 @@ import rutas from '../../router';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 const PrivateRoute = ({ component: Component, ...rest }) => {
-  const { isAuthenticated } = useAuth();
-  return (
-    <Route
-      {...rest}
-      render={(props) =>
-        isAuthenticated ? <Component {...props} /> : <Redirect to={RouteConstant.LOGIN} />
-      }
-    />
-  );
+	const { isAuthenticated } = useAuth();
+	return (
+		<Route
+			{...rest}
+			render={(props) =>
+				isAuthenticated ? (
+					<Component {...props} />
+				) : (
+					<Redirect to={RouteConstant.LOGIN} />
+				)
+			}
+		/>
+	);
 };
 
 const LoginRoute = ({ component: Component, ...rest }) => {
-  const { isAuthenticated } = useAuth();
-  return isAuthenticated ? <Redirect to={RouteConstant.INICIO} /> : <Route {...rest} component={Component} />;
+	const { isAuthenticated } = true;
+	return isAuthenticated ? (
+		<Redirect to={RouteConstant.INICIO} />
+	) : (
+		<Route {...rest} component={Component} />
+	);
+	//const { isAuthenticated } = useAuth();
 };
 
 const RedirectRoute = ({ ...rest }) => {
-  const { isAuthenticated } = useAuth();
-  return <Redirect to={isAuthenticated ? RouteConstant.INICIO : RouteConstant.LOGIN} />;
+	const { isAuthenticated } = useAuth();
+	return (
+		<Redirect
+			to={isAuthenticated ? RouteConstant.INICIO : RouteConstant.LOGIN}
+		/>
+	);
 };
 
 const routes = [...rutas];
 
 const RutasReact = routes.map((item, index) => (
-  <PrivateRoute exact path={item.path} component={item.component} key={index} />
+	<PrivateRoute exact path={item.path} component={item.component} key={index} />
 ));
 
 const App = () => {
-  return (
-    <Fragment>
-       <ToastContainer />
-      <AuthProvider>
-        <Router>
-         
-          <Switch>
-            <LoginRoute path={RouteConstant.LOGIN} component={Login} />
-            {RutasReact}
-            <RedirectRoute />
-          </Switch>
-        </Router>
-      </AuthProvider>
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<ToastContainer />
+			<AuthProvider>
+				<Router>
+					<Switch>
+						<LoginRoute path={RouteConstant.LOGIN} component={Login} />
+						{RutasReact}
+						<RedirectRoute />
+					</Switch>
+				</Router>
+			</AuthProvider>
+		</Fragment>
+	);
 };
 
 export default App;
