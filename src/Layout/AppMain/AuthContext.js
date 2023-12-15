5ab69541b838469c65dcd96ded4bfd59f1c235dd
IsaacGalindoBarbina <20203tn019@utez.edu.mx>
2023-12-14 22:51:10 -0600

    changes

--- src/Layout/AppMain/AuthContext.js
+++ src/Layout/AppMain/AuthContext.js
@@ -3,28 +3,29 @@ import React, { createContext, useContext, useState } from 'react';
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(() => {
-    return localStorage.getItem('isAuthenticated') === 'true';
-  });
+	const [isAuthenticated, setIsAuthenticated] = useState(() => {
+		return true;
+		//return localStorage.getItem('isAuthenticated') === 'true';
+	});
 
-  const login = () => {
-    localStorage.setItem('isAuthenticated', 'true');
-    setIsAuthenticated(true);
-  };
+	const login = () => {
+		localStorage.setItem('isAuthenticated', 'true');
+		setIsAuthenticated(true);
+	};
 
-  const logout = () => {
-    localStorage.setItem('isAuthenticated', 'false');
-    localStorage.clear();
-    setIsAuthenticated(false);
-  };
+	const logout = () => {
+		localStorage.setItem('isAuthenticated', 'false');
+		localStorage.clear();
+		setIsAuthenticated(false);
+	};
 
-  return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
-      {children}
-    </AuthContext.Provider>
-  );
+	return (
+		<AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+			{children}
+		</AuthContext.Provider>
+	);
 };
 
 export const useAuth = () => {
-  return useContext(AuthContext);
+	return useContext(AuthContext);
 };
